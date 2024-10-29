10311de109e0bcdb2b28ad7acf834ffaca4fa378
hilal <hilalhidayah589456@gmail.com>
2024-10-30 01:28:22 +0700

    third. create new folder for models and add a schema and model in it

---
+++ backend/models/bookModel.js
@@ -0,0 +1,24 @@
+// karena mongoose menggunakan model, membuat model di file index.js kurang bagus jadi perlu adanya folder khusus untuk model ini biar ga crowded
+import mongoose, { Schema } from "mongoose";
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publisherYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Book = mongoose.model("Book", bookSchema);
