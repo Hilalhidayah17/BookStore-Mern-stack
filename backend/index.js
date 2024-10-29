d57f88e4faafdaed110c74a52cd357e99c7f1ec1
hilal <hilalhidayah589456@gmail.com>
2024-10-30 02:28:21 +0700

    5.Get all book

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,20 @@ const app = e();
 app.use(bodyParser.json());
 app.get("/", (req, res) => {});
 
+// Route for get All books from database
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.error();
+    res.status(500).send({ message: error.message });
+  }
+});
+
 // Route for save/create a new book using POST
 app.post("/books", async (req, res) => {
   try {
