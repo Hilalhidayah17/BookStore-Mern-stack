1c18eb681c4fce91d5e5d040e91cce8194f1ca64
hilal <hilalhidayah589456@gmail.com>
2024-10-30 01:13:25 +0700

    added some comment for second commit

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,12 @@ const app = e();
 
 app.get("/", (req, res) => {});
 
+// connect database using mongoose
 mongoose
   .connect(mongoDBURL)
   .then(() => {
     console.log("App connected to database");
+    // run server only if database has connected
     app.listen(PORT, () => {
       console.log(`App is running on port: ${PORT}`);
     });
