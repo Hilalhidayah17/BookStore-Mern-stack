ed9dd12c34412af4836c018632b8332ebf0fea1f
hilal <hilalhidayah589456@gmail.com>
2024-10-30 20:57:15 +0700

    add comment in config Js

--- backend/config.js
+++ backend/config.js
@@ -4,4 +4,4 @@ export const mongoDBURL =
   "mongodb+srv://hilal589:darak589789@book-store-mern.i9dqw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern";
 
 //   create free database
-// this data will be delete after tutuorial
+// this data will be delete after tutuorials
