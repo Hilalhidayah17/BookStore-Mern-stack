34cfc7e8f852354a6701f3ff7ec902d7ee5a63bc
hilal <hilalhidayah589456@gmail.com>
2024-11-03 04:22:52 +0700

    14. Add a delete feature

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
 
 export default function DeleteBook() {
-  return <div>DeleteBook</div>;
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:3000/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are you sure to delete this book?</h3>
+        <button
+          className="bg-red-500 p-4 text-white my-4 w-full rounded-xl"
+          onClick={handleDeleteBook}
+        >
+          Yes
+        </button>
+        <Link to={"/"} className="w-full">
+          <button className="bg-gray-200 w-full rounded-xl p-4">No</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
